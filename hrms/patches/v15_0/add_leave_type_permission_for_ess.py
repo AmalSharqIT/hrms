676602625481844a64356831d33ed21737ae3ba0
AmalSharqIT <AmalSharq.IT@gmail.com>
2026-02-03 21:27:00 +0000

    disable add_leave_type_permission_for_ess patch

--- hrms/patches/v15_0/add_leave_type_permission_for_ess.py
+++ hrms/patches/v15_0/add_leave_type_permission_for_ess.py
@@ -2,16 +2,4 @@ import frappe
 
 
 def execute():
-	doc = frappe.get_doc("User Type", "Employee Self Service")
-
-	existing = {d.document_type for d in doc.user_doctypes}
-
-	if "Leave Type" not in existing:
-		doc.append(
-			"user_doctypes",
-			{
-				"document_type": "Leave Type",
-				"read": 1,
-			},
-		)
-		doc.save()
+	pass
