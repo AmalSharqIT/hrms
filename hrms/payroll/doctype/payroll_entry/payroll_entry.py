12a7e4b7f825693773b714c3bd5b71d288e0b909
Amal Sharq <161139587+AmalSharqIT@users.noreply.github.com>
2026-02-09 12:54:45 +0300

    feat: sort employees in payroll-entry (#99)
    Co-authored-by: elshafei-developer <el.shafei.developer@gmail.com>

--- hrms/payroll/doctype/payroll_entry/payroll_entry.py
+++ hrms/payroll/doctype/payroll_entry/payroll_entry.py
@@ -1297,6 +1297,7 @@ def get_filtered_employees(
 			& (SalaryStructureAssignment.payroll_payable_account == filters.payroll_payable_account)
 			& (filters.end_date >= SalaryStructureAssignment.from_date)
 		)
+		.orderby(Employee.employee_name)
 	)
 
 	query = set_fields_to_select(query, fields)
