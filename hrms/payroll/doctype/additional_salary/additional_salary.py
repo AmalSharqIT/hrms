c164bb841fd5fe7018312ed954efa7b62405d141
elshafei-developer <el.shafei.developer@gmail.com>
2026-07-15 14:04:31 +0000

    fix: add check if ref_doctype == "Employee Advance"

--- hrms/payroll/doctype/additional_salary/additional_salary.py
+++ hrms/payroll/doctype/additional_salary/additional_salary.py
@@ -278,7 +278,7 @@ class AdditionalSalary(Document):
 		if self.ref_doctype == "Employee Referral":
 			status = "Unpaid" if cancel else "Paid"
 			frappe.db.set_value("Employee Referral", self.ref_docname, "referral_payment_status", status)
-		else:
+		elif self.ref_doctype == "Employee Advance":
 			frappe.get_doc("Employee Advance", self.ref_docname).set_status(True)
 
 	def get_amount(self, sal_start_date, sal_end_date):
