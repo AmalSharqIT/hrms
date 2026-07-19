fee0642a2f2c7b49653cdfbdcb7b2c50986a382a
elshafei-developer <el.shafei.developer@gmail.com>
2026-07-19 06:47:45 +0000

    feat: Transportation Allowance to Salary Structure Assignment

--- hrms/payroll/doctype/payroll_settings_salary_component/payroll_settings_salary_component.py
+++ hrms/payroll/doctype/payroll_settings_salary_component/payroll_settings_salary_component.py
@@ -2,4 +2,28 @@ from frappe.model.document import Document
 
 
 class PayrollSettingsSalaryComponent(Document):
+	# begin: auto-generated types
+	# This code is auto-generated. Do not modify anything in this block.
+
+	from typing import TYPE_CHECKING
+
+	if TYPE_CHECKING:
+		from frappe.types import DF
+
+		currency: DF.Link
+		parent: DF.Data
+		parentfield: DF.Data
+		parenttype: DF.Data
+		salary_component: DF.Link
+		type: DF.Literal[
+			"Incentive",
+			"Loan Installment",
+			"Penalty",
+			"Reward",
+			"Rounding",
+			"Withholding",
+			"Transportation Allowance",
+		]
+	# end: auto-generated types
+
 	pass
