d58884ecb67229822ac20e16beaa5d3f3f5aab84
Amal Sharq <161139587+AmalSharqIT@users.noreply.github.com>
2026-07-22 16:45:26 +0330

    feat: add Maximum Withholding Balance field in Payroll Settings (#178)
    Co-authored-by: elshafei-developer <el.shafei.developer@gmail.com>

--- hrms/payroll/doctype/payroll_settings_withholding/payroll_settings_withholding.py
+++ hrms/payroll/doctype/payroll_settings_withholding/payroll_settings_withholding.py
@@ -2,4 +2,20 @@ from frappe.model.document import Document
 
 
 class PayrollSettingsWithholding(Document):
+	# begin: auto-generated types
+	# This code is auto-generated. Do not modify anything in this block.
+
+	from typing import TYPE_CHECKING
+
+	if TYPE_CHECKING:
+		from frappe.types import DF
+
+		currency: DF.Link
+		maximum_withholding_balance: DF.Currency
+		minimum_amount: DF.Currency
+		parent: DF.Data
+		parentfield: DF.Data
+		parenttype: DF.Data
+	# end: auto-generated types
+
 	pass
