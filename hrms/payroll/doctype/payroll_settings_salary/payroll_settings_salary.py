4ad5fecfa040fa0c590e432aee108ba367d2d318
AmalSharqIT <AmalSharq.IT@gmail.com>
2026-08-14 20:33:55 +0000

    Update: Customization 2026-08-13

---
+++ hrms/payroll/doctype/payroll_settings_salary/payroll_settings_salary.py
@@ -0,0 +1,22 @@
+from frappe.model.document import Document
+
+
+class PayrollSettingsSalary(Document):
+	# begin: auto-generated types
+	# This code is auto-generated. Do not modify anything in this block.
+
+	from typing import TYPE_CHECKING
+
+	if TYPE_CHECKING:
+		from frappe.types import DF
+
+		currency: DF.Link
+		maximum_withholding_balance: DF.Currency
+		minimum_withholding_amount: DF.Currency
+		parent: DF.Data
+		parentfield: DF.Data
+		parenttype: DF.Data
+		round_salary: DF.Currency
+	# end: auto-generated types
+
+	pass
