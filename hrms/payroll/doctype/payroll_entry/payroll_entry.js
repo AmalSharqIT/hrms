9a013d6ae6a84049c41fd4e64085e8d75406035e
Amal Sharq <161139587+AmalSharqIT@users.noreply.github.com>
2026-08-26 14:19:43 +0330

    update: remove custom_button Submit Salary Slip (#194)
    Co-authored-by: elshafei-developer <el.shafei.developer@gmail.com>

--- hrms/payroll/doctype/payroll_entry/payroll_entry.js
+++ hrms/payroll/doctype/payroll_entry/payroll_entry.js
@@ -167,10 +167,6 @@ frappe.ui.form.on("Payroll Entry", {
 			(frm.doc.__onload && frm.doc.__onload.submitted_ss)
 		) {
 			frm.events.add_bank_entry_button(frm);
-		} else if (frm.doc.salary_slips_created && frm.doc.status !== "Queued") {
-			frm.add_custom_button(__("Submit Salary Slip"), function () {
-				submit_salary_slip(frm);
-			}).addClass("btn-primary");
 		} else if (!frm.doc.salary_slips_created && frm.doc.status === "Failed") {
 			frm.add_custom_button(__("Create Salary Slips"), function () {
 				frm.trigger("create_salary_slip");
